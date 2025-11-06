568583bcd30f21a78df965c6860157907f46b46f
jeshemy <jeshemymarie@gmail.com>
2025-11-06 21:18:54 +0800

    Initial commit

---
+++ OOP-PROJ-master/OOPfinalproj-master/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module oop.tanregister.register {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.logging;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.core;
+
+    opens oop.tanregister.register to javafx.fxml;
+    exports oop.tanregister.register;
+}
